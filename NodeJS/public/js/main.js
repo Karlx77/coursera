dd40ffbf2af09c04692b1a09038dcd2694cc4ab2
Karlx77 <katniss_12_ja@outlook.es>
2020-09-16 10:57:37 -0400

    colocando eventos en el modal

--- NodeJS/public/js/main.js
+++ NodeJS/public/js/main.js
@@ -4,4 +4,18 @@ $(function(){
     $('.carousel').carousel({
         interval: 2000
     });
+    $('#exampleModal').on('shown.bs.modal', function (e){
+        console.log("el modal se mostro");
+    })
+    $('#exampleModal').on('show.bs.modal', function (e){
+        $('#modal1').removeClass('btn-outline-warning')
+        $('#modal1').addClass('btn-outline-success')        
+    })
+    $('#exampleModal').on('hide.bs.modal', function (e){
+        console.log("se oculta")
+    })
+    $('#exampleModal').on('hidden.bs.modal', function (e){
+        $('#modal1').addClass('btn-outline-warning') 
+
+    })
 })
